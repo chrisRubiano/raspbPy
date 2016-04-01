6d94d522f3553bb8c4132b66786baaad0aba1a22
Jonathan Guillermo <hernandezmarquina@gmail.com>
2016-04-01 13:33:33 -0700

    first commit

---
+++ raspbpy/raspbpy/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for raspbpy project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "raspbpy.settings")
+
+application = get_wsgi_application()
